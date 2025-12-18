27a115b3bc0d7cae1b65d364b11fa31f4c17104a
Ania <anna.szczolko@gmail.com>
2025-12-18 14:40:25 +0100

    Styling changes

--- frontend/src/components/UserItem.jsx
+++ frontend/src/components/UserItem.jsx
@@ -32,24 +32,5 @@ export default function UserItem({ user }) {
 			</tr>
 		
 
-		// 	<div className='user'>
-		// 		<div className="user-main">
-		// 		<p>{data.name} {data.surname}</p>
-
-		// 		</div>
-
-		// 		<p>{data.gender}</p>
-		// 		<p>{data.age}</p>
-		// 		<ul className='occupation-list'>
-		// 			{data.occupation.map(occ => (
-		// 				<li className='occupation' key={occ}>{occ}</li>
-		// 			))}
-		// 		</ul>
-		// 		<div className="user-actions">
-		//   <button className='button' onClick={() => deleteUserHandler(data.id)}>Delete</button>
-		//   <button className='button button-primary' onClick={() => changeUserDataHandler(data.id)}>Change Data</button>
-
-		// 		</div>
-		// 	</div>
 	)
 }
